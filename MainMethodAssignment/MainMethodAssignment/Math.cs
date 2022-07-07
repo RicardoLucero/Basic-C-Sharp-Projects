7ff5c7c6698f197ccb10af0deccc0a177b74abbc
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-07 14:47:27 -0700

    Console App with method overloading
    Created a class with 3 methods of the same name to practice method overloading. The method asks for different parameters for the overloading to work correctly. We then instantiate the method in the main program.

---
+++ MainMethodAssignment/MainMethodAssignment/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment
+{
+    class Math
+    {
+        public static int MyMethod(int x)//the parameter is an int
+        {
+            int total = 50 + x;// the operation is addition to the input
+            return total;// we return an int
+        }
+
+        public static int MyMethod(decimal y)//the parameter is a decimal but the method wants an int returned
+        {
+            int total = Convert.ToInt32(y) * 7;//we cast the parameter/argument to an int
+            return total;// now we can return an int
+        }
+
+        public static int MyMethod(string z)//the parameter is a string but the method will return an int
+        {
+            int total = 500 - Convert.ToInt32(z);//the total var is an int so we cast the string to an int to work with it
+            return total;// Return the int to the method
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give a number.");// Ask for input
+            int input = Convert.ToInt32(Console.ReadLine());// Cast to int to work with our method
+            int total = Math.MyMethod(input);// Pass int input to the method 
+            Console.WriteLine(input + " plus " + 50 + " equals " + total);// Write the total to the console
+            Console.ReadLine();
+
+            Console.WriteLine("Please give a decimal number.");// Ask for a decimal input this time
+            decimal input2 = Convert.ToDecimal(Console.ReadLine());// Cast this time to a decimal
+            int total2 = Math.MyMethod(input2);// the variable is an int since we return an int from the method
+            Console.WriteLine("You answer will be rounded to a whole number :" + total2);
+            Console.ReadLine();
+
+            Console.WriteLine("Please give a number.");// The input askk for a number but we take it as a string
+            string input3 = Console.ReadLine();// We take the number as a string
+            int total3 = Math.MyMethod(input3);// Our method returns an int
+            Console.WriteLine(input3 + " minus " + 500 + " equals " + total3);
+            Console.ReadLine();
+        }
+    }
+}
