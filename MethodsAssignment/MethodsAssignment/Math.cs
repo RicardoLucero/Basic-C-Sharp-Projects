c7d71efe16af831e4e90eed059d9fa113519a71f
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-06 17:22:30 -0700

    Create a couple of methods
    Created three methods in a separate math class and called them in our main program. Created a multiply, divide, and addition method. all we need is a single integer parameter and we call each method to find out the totals.

---
+++ MethodsAssignment/MethodsAssignment/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Math
+    {
+        //we make the method public to use it in the main program
+        //also make it static to make it return an integer
+        public static int MultiplyMethod(int number)//the parameter is going to be an int variable
+        {
+            int total = 9 * number;// we multiply the variable by 9 to do the operation
+            Console.WriteLine(9 + " times " + number + " equals: " + total);
+            return total;//we return the interger total
+        }//we do the same for the divide method
+        public static int DivideMethod(int number)
+        {
+            int total = 365 / number;
+            Console.WriteLine(12 + " divided by " + number + " equals: " + total);
+            return total;
+        }//same goes for add method
+        public static int AddMethod(int number)
+        {
+            int total = 50 + number;
+            Console.WriteLine(50 + " plus " + number + " equals: " + total);
+            return total;
+        }
+    }
+}
---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //We ask the user for an input
+            Console.WriteLine("Please enter a number to do some mathmatical operations with.");
+            int number = Convert.ToInt32(Console.ReadLine());//cast the input to an int to work some math operations with
+            Console.WriteLine(Math.MultiplyMethod(number));//this is how we call the method and print it to the console
+            Console.ReadLine();//we use a readline in between to give the user time to read what the method did to their input
+            Console.WriteLine(Math.DivideMethod(number));
+            Console.ReadLine();
+            Console.WriteLine(Math.AddMethod(number));
+            Console.ReadLine();
+        }
+    }
+}
