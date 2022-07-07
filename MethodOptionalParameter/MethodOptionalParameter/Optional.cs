66dab3486702aea25e2ac35aef7443dc13792f46
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-07 16:06:05 -0700

    console app method with optional parameter
    created a class that holds a method with an optional parameter. In the main program we instantiate the method and ask the user for two inputs. we let them know that the second input is optional. The method is run and returns the total.

---
+++ MethodOptionalParameter/MethodOptionalParameter/Optional.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOptionalParameter
+{
+    class Optional
+    {
+        public static int MyMethod(string x, string y = "6")//the method is asking for two parameters and one is an optional parameter
+        {
+            int total = Convert.ToInt32(y) * Convert.ToInt32(x);// we will multiply the two together
+            return total;//we return the total.
+        }
+    }
+}
---
+++ MethodOptionalParameter/MethodOptionalParameter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodOptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 2 numbers, but 1 at a time." +//We tell the user we  want two inputs
+                "\nEnter the first number now.");//Now we ask for one at a time
+            string x = Console.ReadLine();// the first input is put into the variable x
+            Console.WriteLine("The Second number is optional.");// we now ask for the second input, we let them know its optional
+            string y = Console.ReadLine();// the second input is put into the the variable y
+            if ( y == "") //we use an if statement to get the optional statement to work with an empty 2nd argument
+            {
+                int total = Optional.MyMethod(x);// this method will use the defualt paramenter
+                Console.WriteLine(total);
+            }
+            else // else is used to use the optional parameter
+            {
+                int total = Optional.MyMethod(x, y);// we call the method and pass the arguments over
+                Console.WriteLine(total);
+            }
+        }
+    }
+}
