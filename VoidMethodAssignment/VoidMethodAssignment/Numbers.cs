27605f6760b114c25ff0e0955425984214f5f26e
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-08 09:52:25 -0700

    Created a console app that has a void method. It takes two integer parameters. The first parameter has a math equation done to it. The second parameter is just printed to the screen. We name the two parameters.

---
+++ VoidMethodAssignment/VoidMethodAssignment/Numbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethodAssignment
+{
+    class Numbers
+    {
+        public static void MyMethod(int firstNum, int secondNum)//the method wants two integers as the arguments
+        {
+            int total = 356 / firstNum;// we take the first parameter and run a math equation to it
+            Console.WriteLine(secondNum);// the second parameter we just print to the screen
+        }
+    }
+}
---
+++ VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers mynumbers = new Numbers();// Instantiate the class
+
+            Console.WriteLine("please give two numbers.\n" +//we ask for input of two numbers for the arguments
+                "Enter the first number.");
+            int firstNum = Convert.ToInt32(Console.ReadLine());//cast the input to int
+            Console.WriteLine("Enter the second number.");
+            int secondNum = Convert.ToInt32(Console.ReadLine());//cast the second input to int
+            Numbers.MyMethod(secondNum = firstNum, firstNum = secondNum);// calling the method and naming the parameters
+        }
+    }
+}
