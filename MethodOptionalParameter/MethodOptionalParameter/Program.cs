43c4a729b6888720353498d0cf06e912c418889e
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-08 14:25:23 -0700

    update files

--- MethodOptionalParameter/MethodOptionalParameter/Program.cs
+++ MethodOptionalParameter/MethodOptionalParameter/Program.cs
@@ -12,16 +12,16 @@ namespace MethodOptionalParameter
                 "\nEnter the first number now.");//Now we ask for one at a time
             int x = Convert.ToInt32(Console.ReadLine());// the first input is put into the variable x
             Console.WriteLine("The Second number is optional." +
-                "\n if no number is wanted please enter zero.");// we now ask for the second input, we let them know its optional
-            int y = Convert.ToInt32(Console.ReadLine());// the second input is put into the the variable y
-            if ( y == 0) //we use an if statement to get the optional statement to work with an empty 2nd argument
+                "\n if no number is wanted please press enter.");// we now ask for the second input, we let them know its optional
+            string y = Console.ReadLine();// the second input is put into the the variable y
+            if ( y == "") //we use an if statement to get the optional statement to work with an empty 2nd argument
             {
                 int total = myObj.MyMethod(x);// this method will use the defualt paramenter
                 Console.WriteLine(total);
             }
             else // else is used to use the optional parameter
             {
-                int total = myObj.MyMethod(x, y);// we call the method and pass the arguments over
+                int total = myObj.MyMethod(x, Convert.ToInt32(y));// we call the method and pass the arguments over
                 Console.WriteLine(total);
             }
         }
