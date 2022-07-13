122616f4ba68303f2d3ae148e3631240e86bcb6f
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-13 10:25:10 -0700

    Overload Operators
    Created a console app that uses overloaded operators. Create a class that defines overloaded operators and use the operators in the main method to compare two objects to each other.

---
+++ OverloadingOperators/OverloadingOperators/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadingOperators
+{
+    public class Employee
+    {
+        //set the properties of the class
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //overload the == operator
+        public static bool operator == (Employee person, Employee ID)
+        {           
+            return person.Equals(ID);//we return true if the ID is found
+        }
+        //overload the != operator
+        public static bool operator != (Employee person, Employee ID)
+        {
+            return ! person.Equals(ID);//we return false if the ID is not found
+        }
+    }
+}
---
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OverloadingOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the first object of Employee
+            Employee personOne = new Employee() { ID = 001, FirstName = "Josh", LastName = "Myers" };//intialize the first object
+
+            //Instantiate the second object of Employee
+            Employee personTwo = new Employee() { ID = 002, FirstName = "Bryan", LastName = "Cranston" };//initialize the second object
+
+            //we use the overloaded operators to check if the employees are the same
+            if (personOne.ID == personTwo.ID)
+                Console.WriteLine("This is the same employee");
+            if (personOne.ID != personTwo.ID)
+                Console.WriteLine("This is not the same employee");
+        }
+    }
+}
