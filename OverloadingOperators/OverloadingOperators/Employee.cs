63e05ce8fcd9d10807325c0f2b6f32416b974bc9
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-13 13:23:33 -0700

    correct assignment
    correct some points that were missed in the assignment

--- OverloadingOperators/OverloadingOperators/Employee.cs
+++ OverloadingOperators/OverloadingOperators/Employee.cs
@@ -11,14 +11,18 @@ namespace OverloadingOperators
         public string FirstName { get; set; }
         public string LastName { get; set; }
         //overload the == operator
-        public static bool operator == (Employee person, Employee ID)
-        {           
-            return person.Equals(ID);//we return true if the ID is found
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            if (employee1.ID == employee2.ID)
+                Console.WriteLine("This is the same employee.");
+            return true;//we return true if the ID is found
         }
         //overload the != operator
-        public static bool operator != (Employee person, Employee ID)
+        public static bool operator != (Employee employee1, Employee employee2)
         {
-            return ! person.Equals(ID);//we return false if the ID is not found
+            if (employee1.ID != employee2.ID)
+                Console.WriteLine("This is not the same employee.");
+            return false;//we return false if the ID is not found
         }
     }
 }
--- OverloadingOperators/OverloadingOperators/Program.cs
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -7,16 +7,20 @@ namespace OverloadingOperators
         static void Main(string[] args)
         {
             //Instantiate the first object of Employee
-            Employee personOne = new Employee() { ID = 001, FirstName = "Josh", LastName = "Myers" };//intialize the first object
+            Employee employee1 = new Employee() { ID = 1, FirstName = "Josh", LastName = "Myers" };//intialize the first object
 
             //Instantiate the second object of Employee
-            Employee personTwo = new Employee() { ID = 002, FirstName = "Bryan", LastName = "Cranston" };//initialize the second object
+            Employee employee2 = new Employee() { ID = 2, FirstName = "Bryan", LastName = "Cranston" };//initialize the second object
 
             //we use the overloaded operators to check if the employees are the same
-            if (personOne.ID == personTwo.ID)
-                Console.WriteLine("This is the same employee");
-            if (personOne.ID != personTwo.ID)
-                Console.WriteLine("This is not the same employee");
+            if (employee1 != employee2)
+            {
+                Console.WriteLine(employee1 != employee2);
+            }
+            else
+            {
+                Console.WriteLine(employee1 == employee2);
+            }
         }
     }
 }
