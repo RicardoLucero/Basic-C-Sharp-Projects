a3320d7f80b22d228bf9fdd5ae6828aef34c02ea
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-14 12:53:35 -0700

    Create an Enums app
    Created an app that utilized Enums. create an Enum and used it in our main() method to ask the user for the day of the week. Wrapped the whole thing in a try/catch block to handle any errors.

---
+++ ParsingEnums/ParsingEnums/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParsingEnums
+{
+    public class Enums
+    {
+        //create our enum of days of the week
+        public enum DaysOfTheWeek 
+        {
+            //create the constants of the enum
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a try/catch block to handle exceptions if user inputs incorrectly
+            try
+            {
+                //Ask for an input
+                Console.WriteLine("Please enter the current day of the week.");
+                string day = Console.ReadLine();//place the input in a string variable
+                Enums.DaysOfTheWeek days = (Enums.DaysOfTheWeek)Enum.Parse(typeof(Enums.DaysOfTheWeek), day);//convert the string to enum varable days.
+            }
+            //our catch block cathces if there is an exceptions and throws an error message
+            catch (Exception ex)
+            {
+                //Wrote our own message instead of the defualt message
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
