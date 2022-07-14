e50865038f80351de1b77e0b4d86b77fdcde5c1c
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-13 21:33:48 -0700

    Create a generic class app
    Created a class with a generic list data type. called the class to instantiate the list with certain data types. Then, used a foreach loop to iterate through the lists and print the items to the console.

---
+++ GenericParameters/GenericParameters/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericParameters
+{
+    //create a generic class
+    public class Employee<T>//The T is the generic keyword to let the list take a generic data type
+    {
+        //the data type is generic to allow the class that calls upon it to define the type
+        public List<T> things { get; set; }
+    }
+}
---
+++ GenericParameters/GenericParameters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the list with the data type parameter
+            Employee<string> items = new Employee<string>();
+            //create an object of the List
+            items.things = new List<string>();
+            //add the items to the List
+            items.things.Add("keys");
+            items.things.Add("phone");
+            items.things.Add("wallet");
+            items.things.Add("lunchbag");
+            items.things.Add("badge");
+            //instantiate the list with the data type parameter
+            Employee<int> numbers = new Employee<int>();
+            //create an object of the list
+            numbers.things = new List<int>();
+            //add the numbers to the list
+            numbers.things.Add(47);
+            numbers.things.Add(99);
+            numbers.things.Add(666);
+            numbers.things.Add(777);
+            numbers.things.Add(69);
+            // create a foreach loop to iterate through the list
+            foreach (string item in items.things)
+            {//write each item to the console as you iterate through the list
+                Console.WriteLine("You have a " + item);
+            }
+            // create another foreach loop for the second list
+            foreach (int number in numbers.things)
+            {
+                Console.WriteLine("You're number is " + number);
+            }
+        }
+    }
+}
