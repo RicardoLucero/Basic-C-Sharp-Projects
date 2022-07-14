09221eefaaad6163c1961f4e94f8e6d205041a3a
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-14 13:53:29 -0700

    Create an app with struct
    Created an app that utilized a struct. Instantiated an object from the struct and assigned it a value. lastly, printed the object to the console.

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructAssignment
+{
+    //Create a struct with the keyword
+    public struct Number
+    {
+        //giving it the property amount with the data type of decimal
+        public decimal Amount;
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an instance of the struct Number
+            Number number;
+
+            //assigning a value to the number struct
+            number.Amount = 47595;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
