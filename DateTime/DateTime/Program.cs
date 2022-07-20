ad548b0a869e8964567f3b9b8887aa4e5164ef2a
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-20 11:53:08 -0700

    Create an app that uses DateTime
    created an app that post the current time. then we ask for input from the user and uses the input to add time to the current time and post it to the console.

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace DateTime1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome to my app the current time is: " + DateTime.Now);//print to the console the current time
+            Console.WriteLine("Please input a number.");//ask for input from the user
+            int x = Convert.ToInt32(Console.ReadLine());// cast the input to an integer
+            DateTime currentTime = DateTime.Now.AddHours(x);//addthe input to the current time
+            Console.WriteLine("Your number " + x + " will be added to the time to let you know what time it will be. " + currentTime);// let the user know what time it will be with their input
+            
+        }
+    }
+}
