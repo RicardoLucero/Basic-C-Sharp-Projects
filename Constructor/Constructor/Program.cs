e389414ba1f8fbab93ef4c34c7ab2df7940a0be8
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-07-20 16:01:37 -0700

    Create a constructor app
    Created an app that uses a constant and uses a var varibales and chains constructors.

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string appName = "Constructors";//create a constant of the app name Constructors
+            Console.WriteLine("Welcome to the {0} app.", appName);// we do a string format with the constant
+            Console.WriteLine("Please tell me your name?");// we ask for an input of the user
+            var userName = Console.ReadLine();// place the input in a variable of var
+            Console.WriteLine("Please tell me which city you are from.");
+            var city = Console.ReadLine();
+            User user = new User(userName, city);//place the varibles in the user method
+        }
+    }
+}
---
+++ Constructor/Constructor/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor
+{
+    class User
+    {
+        //create a class of user to chain constructors
+        public string userName;
+        public string city;
+
+        public User (string userName) : this(userName, "Los Angeles")// this method takes only one construct and the second one is a default value
+        {
+            //it does not need anyting in the curly braces as it chains off the other constructors 
+        }
+
+        public User (string userName, string city)//this takes two constructors
+        {
+            this.userName = userName;
+            this.city = city;
+        } 
+    }
+}
